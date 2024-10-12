9325c92cfa2578cd32a7f20a0865c72b6c058f3d
Saitto <68688545+SaittoXV@users.noreply.github.com>
2024-10-12 14:45:43 +0800

    Update Motor Movement.ino

--- Motor Movement.ino
+++ Motor Movement.ino
@@ -1,29 +1,37 @@
+// Motor
 int right_Motor_Direction = 10;
 int right_Motor_Speed = 11;
 int left_Motor_Direction = 5;
 int left_Motor_Speed = 3;
 
+//Infra-Red Sensor LSS05
+int far_Left_Sensor = A1;
+int left_Sensor = A2;
+int middle_Sensor = A3;
+int right_Sensor = A4;
+int far_Right_Sensor = A5;
+
 void setup()
 {
+  //Motor
   pinMode(right_Motor_Direction,OUTPUT);
   pinMode(right_Motor_Speed,OUTPUT);
   pinMode(left_Motor_Direction,OUTPUT);
   pinMode(left_Motor_Speed,OUTPUT);
+
+  //Infra-Red Sensor
+  pinMode(far_Left_Sensor,INPUT);
+  pinMode(left_Sensor,INPUT);
+  pinMode(middle_Sensor,INPUT);
+  pinMode(right_Sensor,INPUT);
+  pinMode(far_Right_Sensor,INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  
-  forward(200,200);
-  delay(1000);
-  backward(200,200);
-  delay(1000);
-  right(100,200);
-  delay(1000);
-  left(200,100);
-  delay(1000);
-  stop();
-  delay(10000);
+
 
 }
 
