4a8c007be6917cd5b4c05062b52d9470dde7923b
Saitto <68688545+SaittoXV@users.noreply.github.com>
2024-12-08 09:16:43 +0800

    Update Line Following (Student).ino

--- Line Following (Student).ino
+++ Line Following (Student).ino
@@ -1,87 +1,198 @@
 // Motor
-int right_Direction = 10;
-int right_Speed = 11;
-int left__Direction = 5;
-int left_Speed = 3;
+int right_Motor_Direction = 10;
+int right_Motor_Speed = 11;
+int left_Motor_Direction = 5;
+int left_Motor_Speed = 3;
 
 //Infra-Red Sensor LSS05
-int S1 = A1;
-int S2 = A2;
-int S3 = A3;
-int S4 = A4;
-int S5 = A5;
+int far_Left_Sensor = A1;
+int left_Sensor = A2;
+int middle_Sensor = A3;
+int right_Sensor = A4;
+int far_Right_Sensor = A5;
+
+// Button Pin
+int button_Pin = 7;
+// Junction Count
+int junction_Count = 0;
 
 /* ---------------------- SETUP FUNCTION ---------------------- */
 void setup()
 {
   //Motor
-
+  pinMode(right_Motor_Direction,OUTPUT);
+  pinMode(right_Motor_Speed,OUTPUT);
+  pinMode(left_Motor_Direction,OUTPUT);
+  pinMode(left_Motor_Speed,OUTPUT);
 
   //Infra-Red Sensor
+  pinMode(far_Left_Sensor,INPUT);
+  pinMode(left_Sensor,INPUT);
+  pinMode(middle_Sensor,INPUT);
+  pinMode(right_Sensor,INPUT);
+  pinMode(far_Right_Sensor,INPUT);
 
+  //Button
+  pinMode(button_Pin,INPUT);
 
   Serial.begin(9600);
+
+  int read_Button = digitalRead(button_Pin);
+
+  while(read_Button == LOW)
+  {
+    read_Button = digitalRead(button_Pin);
+  }
 }
 
 /* ---------------------- LOOP FUNCTION ---------------------- */
 void loop()
 {
-  
-  int detect_S1 = digitalRead(S1);
-  int detect_S2 = digitalRead(S2);
-  int detect_S3 = digitalRead(S3);
-  int detect_S4 = digitalRead(S4);
-  int detect_S5 = digitalRead(S5);
 
-  //Start Code Here
+  // Read Line Sensor 
+  int detect_far_left = digitalRead(far_Left_Sensor);
+  int detect_left = digitalRead(left_Sensor);
+  int detect_middle = digitalRead(middle_Sensor);
+  int detect_right = digitalRead(right_Sensor);
+  int detect_far_right = digitalRead(far_Right_Sensor);
 
-  
+  //Junction
+ if (detect_far_left == HIGH && detect_left == HIGH && detect_middle == HIGH && detect_right == HIGH && detect_far_right == HIGH)
+  {
+    Serial.println("Junction");
+    
+    junction_Count = junction_Count+1;
+    forward();
+    stop();
+    delay(100);
+
+    junction(junction_Count);
+
+  }
+  //Right
+  else if((detect_right == HIGH && detect_far_right == HIGH) || detect_far_right == HIGH)
+  {
+    Serial.println("Right");
+    right(40,120);
+    delay(90);
+  }
+  //Small Right
+  else if((detect_right == HIGH && detect_middle == HIGH) || detect_right == HIGH)
+  {
+    Serial.println("Small Right");
+    right(50,100);
+    delay(50);
+  }
+  //Left
+  else if((detect_far_left == HIGH && detect_left == HIGH) || detect_far_left == HIGH )
+  {
+    Serial.println("Left");
+    left(120,40);
+    delay(90);
+  }
+  //Small Left
+  else if((detect_left == HIGH && detect_middle == HIGH) || detect_left == HIGH)
+  {
+    Serial.println("Small Left");
+    left(100,50);
+    delay(50);
+  }
+  //Forward
+  else if(detect_middle == HIGH)
+  {
+    Serial.println("Forward");
+    forward();
+  }
+
 }
 
 /* ---------------------- FORWARD FUNCTION ---------------------- */
 void forward()
 {
   //Right Motor
-  digitalWrite(right_Direction,LOW);
-  analogWrite(right_Speed,100);
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,100);
   
   //Left Motor
-  digitalWrite(left__Direction,LOW);
-  analogWrite(left_Speed,100);
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,100);
+
+  delay(50);
 }
 
 /* ---------------------- RIGHT FUNCTION ---------------------- */
 void right(int speed_Right, int speed_Left)
 {
   //Right Motor
-  digitalWrite(right_Direction,LOW);
-  analogWrite(right_Speed,speed_Right);
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,speed_Right);
   
   //Left Motor
-  digitalWrite(left__Direction,LOW);
-  analogWrite(left_Speed,speed_Left);
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,speed_Left);
+}
+
+/* ---------------------- TURN RIGHT FUNCTION ---------------------- */
+void turn_Right()
+{
+  //Right Motor
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,0);
+  
+  //Left Motor
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,100);
+
+  delay(750);
 }
 
 /* ---------------------- LEFT FUNCTION ---------------------- */
 void left(int speed_Right, int speed_Left)
 {
   //Right Motor
-  digitalWrite(right_Direction,LOW);
-  analogWrite(right_Speed,speed_Right);
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,speed_Right);
   
   //Left Motor
-  digitalWrite(left__Direction,LOW);
-  analogWrite(left_Speed,speed_Left);
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,speed_Left);
+  
+}
+
+/* ---------------------- TURN LEFT FUNCTION ---------------------- */
+void turn_Left()
+{
+  //Right Motor
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,100);
+  
+  //Left Motor
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,0);
+
+  delay(750);
 }
 
 /* ---------------------- STOP FUNCTION ---------------------- */
 void stop()
 {
   //Right Motor
-  digitalWrite(right_Direction,LOW);
-  analogWrite(right_Speed,LOW);
+  digitalWrite(right_Motor_Direction,LOW);
+  analogWrite(right_Motor_Speed,LOW);
   
   //Left Motor
-  digitalWrite(left__Direction,LOW);
-  analogWrite(left_Speed,LOW);
+  digitalWrite(left_Motor_Direction,LOW);
+  analogWrite(left_Motor_Speed,LOW);
+  
+}
+
+/* ---------------------- JUNCTION FUNCTION ---------------------- */
+void junction(int junction_Count)
+{
+  if(junction_Count == 1)
+  {
+    // Start your code here...
+  }
+
+
 }
