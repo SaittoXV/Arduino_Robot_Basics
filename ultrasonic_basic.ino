887f283ca897027142ea36d82348ce71acb44c2b
Saitto <68688545+SaittoXV@users.noreply.github.com>
2026-04-18 22:44:26 +0800

    Add ultrasonic sensor functionality
    Initialize ultrasonic sensor with trig and echo pins, calculate distance based on pulse duration.

--- ultrasonic_basic.ino
+++ ultrasonic_basic.ino
@@ -28,3 +28,47 @@ void loop() {
 
   delay(200);
 }
+
+
+// -----------------------------------------------------------
+const int trig_pin = 9;
+const int echo_pin = 10;
+float duration,duration_second, sound_speed,distance;
+
+
+void setup() {
+
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+
+}
+
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+
+  duration = pulseIn(echo_pin, HIGH);
+  Serial.print("Duration Pulse: ");
+  Serial.println(duration);
+
+  duration_second = duration*0.000001;
+  sound_speed = 343.0*100.0;
+  distance = (duration_second*sound_speed)/2;
+
+  Serial.println(duration_second);
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(100);
+
+} 
