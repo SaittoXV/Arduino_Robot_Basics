1cf34d07b7944aa0b19a639a7ad6fb892599a4b0
SaittoXV <syed.abubakar73@gmail.com>
2026-09-19 22:13:58 +0800

    test

--- Arduino Code/line_following_analog/line_following_analog.ino
+++ Arduino Code/line_following_analog/line_following_analog.ino
@@ -1,11 +1,3 @@
-/**TEACHING
-1. Create a Line Follow Function
-2. Create logic for pressing Button
-3. Found One Junction Logic
-4. Create Turn Left + Right at Junction Function
-5. Multiple Junction Challenge**/
-
-
 //------ Motor Variable -----
 const int E1 = 10;
 const int M1 = 12;
@@ -18,9 +10,6 @@ const int LS = A0;
 const int RS = A4;
 
 const int motorSpeed = 120;
-const int Button = 2;
-
-int Junction = 0;
 
 void setup()
 {
@@ -35,18 +24,7 @@ void setup()
   pinMode(LS,INPUT);
   pinMode(RS,INPUT);
 
-  //----- Set Button -----
-  pinMode(Button,INPUT);
-
   Serial.begin(9600);
-
-  while(true)
-  {
-    if(digitalRead(Button) == HIGH)
-    {
-      break;
-    }
-  }
 }
 
 //------------Set Action Functions-------------
@@ -63,32 +41,15 @@ void turn_right()
 {
     analogWrite(E1, 100);
     digitalWrite(M1, HIGH);
-    analogWrite(E2, 100);
-    digitalWrite(M2, LOW);
-}
-
-
-void turn_right_junction()
-{
-    analogWrite(E1, 150);
-    digitalWrite(M1, HIGH);
-    analogWrite(E2, 150);
+    analogWrite(E2, 0);
     digitalWrite(M2, LOW);
 }
 
 void turn_left()
 {
-    analogWrite(E1, 100);
+    analogWrite(E1, 0);
     digitalWrite(M1, LOW);
     analogWrite(E2, 100);
-    digitalWrite(M2, HIGH);
-}
-
-void turn_left_junction()
-{
-    analogWrite(E1, 150);
-    digitalWrite(M1, LOW);
-    analogWrite(E2, 150);
     digitalWrite(M2, HIGH);
 }
 
@@ -101,7 +62,8 @@ void stop_brake()
     delay(1000);
 }
 
-void line_follow()
+
+void loop()
 {
   int detect_left = digitalRead(LS);
   int detect_center = digitalRead(CS);
@@ -125,35 +87,6 @@ void line_follow()
     move_forward();
     delay(100);
   }
-  
-}
 
-void junction_detect()
-{
-  if(Junction == 1)
-  {
-    stop_brake();
-    delay(1000);
-    move_forward();
-    delay(250);
-    turn_left_junction();
-    delay(500);
-  }  
-}
-
-void loop()
-{
-  int detect_left = digitalRead(LS);
-  int detect_center = digitalRead(CS);
-  int detect_right = digitalRead(RS);
   
-  if(detect_left == HIGH && detect_center == HIGH && detect_right == HIGH)
-  {
-    Junction++;
-    junction_detect();
-  }
-  else
-  {
-      line_follow();
-  }
 }
